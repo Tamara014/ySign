e32ca07d1bb8455a044c0e54671e250fabe7a0a8
Tamara014 <124172343+Tamara014@users.noreply.github.com>
2023-07-14 15:08:44 +0200

    commit

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".item");
+const hamburger= document.querySelector(".bars");
+const closeIcon= document.querySelector(".fa-xmark");
+const menuIcon = document.querySelector(".fa-bars");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
